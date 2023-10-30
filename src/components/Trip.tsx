85143082820483c78777e23ba6e47bbe98e51949
FRANCESCA FUSCO s310039 <s310039@studenti.polito.it>
2023-10-30 13:49:07 -0500

    trip component

--- src/components/Trip.tsx
+++ src/components/Trip.tsx
@@ -1,10 +1,17 @@
-import { Outlet } from 'react-router-dom';
+import {Link, Outlet, useNavigate, useParams} from 'react-router-dom';
+import {Button} from "react-bootstrap";
 
 function Trip() {
+    const navigate = useNavigate();
+    const { tripId } = useParams();
+
     return (
         <div className="trip-layout">
             <div className="sidebar">
                 {/* Sidebar content goes here */}
+                <Button onClick={() => navigate(`${tripId}/editInfo`)} variant="primary">Edit Info</Button>
+                <Button onClick={() => navigate(`${tripId}/chat`)} variant="primary">Chat</Button>
+                <Button onClick={() => navigate(`${tripId}/editToDo`)} variant="primary">Edit To-Do</Button>
             </div>
             <div className="main-content">
                 <Outlet /> {/* This will render the child routes */}
@@ -12,4 +19,6 @@ function Trip() {
         </div>
     );
 }
+
+
 export default Trip;
