4dd412c7a9f50dd5161ef97debed9dba2b905c15
MATTIA CHIARLE s308472 <s308472@studenti.polito.it>
2023-10-30 00:47:14 -0500

    Added login logic

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,16 @@
-function Login() {
+import { useAppContext } from "../context/appContext";
 
+function Login() {
+  const { supabase } = useAppContext();
+
+  const onSubmit = async () => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: "example@email.com",
+      password: "example-password",
+      options: {
+        redirectTo: "https//example.com/welcome",
+      },
+    });
+  };
 }
 export default Login;
