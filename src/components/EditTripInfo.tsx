d95a17652ba9e8d5fa8a02047ae8371c0ae564d9
MATTIA CHIARLE s308472 <s308472@studenti.polito.it>
2023-10-30 23:20:22 -0500

    Fixed trips

--- src/components/EditTripInfo.tsx
+++ src/components/EditTripInfo.tsx
@@ -1,12 +1,11 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import AppContext from "../context/appContext";
 import { Trip } from "../utils/types";
-import { useParams } from 'react-router-dom';
-import {Button, Row, Form} from "react-bootstrap";
-
+import { useParams } from "react-router-dom";
+import { Button, Row, Form } from "react-bootstrap";
 
 function EditTripInfo() {
-  const { supabase } = useContext(AppContext);
+  const supabase = useContext(AppContext);
   const [trip, setTrip] = useState<Trip | null>();
   const { tripId } = useParams();
 
@@ -15,7 +14,7 @@ function EditTripInfo() {
       const { data, error } = await supabase
         .from("trips")
         .select("*")
-        .eq('id', tripId)
+        .eq("id", tripId)
         .single();
 
       if (error) {
@@ -28,22 +27,24 @@ function EditTripInfo() {
     fetchTrip();
   }, [tripId]);
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement| HTMLSelectElement>) => {
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     if (trip) {
-        setTrip({
-          ...trip,
-          [event.target.name]: event.target.value
-        });
-      }
+      setTrip({
+        ...trip,
+        [event.target.name]: event.target.value,
+      });
+    }
   };
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
 
     const { data, error } = await supabase
-      .from('trips')
+      .from("trips")
       .update(trip)
-      .eq('id', tripId);
+      .eq("id", tripId);
 
     if (error) {
       console.error("Error updating trip:", error);
@@ -58,22 +59,38 @@ function EditTripInfo() {
 
   return (
     <Form onSubmit={handleSubmit}>
-    <Row>
-      <label>
-        Start Date:
-        <input type="date" name="startDate" value={trip.startDate} onChange={handleInputChange} />
-      </label>
-    </Row>
-    <Row>
-      <label>
-        End Date:
-        <input type="date" name="endDate" value={trip.endDate} onChange={handleInputChange} />
-      </label>
-    </Row>
-    <Row>
-      <label>
-        Vehicle:
-        <Form.Select name="vehicle" onChange={(event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => handleInputChange(event)} value={trip.vehicle} >
+      <Row>
+        <label>
+          Start Date:
+          <input
+            type="date"
+            name="startDate"
+            value={trip.startDate}
+            onChange={handleInputChange}
+          />
+        </label>
+      </Row>
+      <Row>
+        <label>
+          End Date:
+          <input
+            type="date"
+            name="endDate"
+            value={trip.endDate}
+            onChange={handleInputChange}
+          />
+        </label>
+      </Row>
+      <Row>
+        <label>
+          Vehicle:
+          <Form.Select
+            name="vehicle"
+            onChange={(
+              event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+            ) => handleInputChange(event)}
+            value={trip.vehicle}
+          >
             <option>{trip.vehicle}</option>
             <option>Car</option>
             <option>Motorbike</option>
@@ -81,9 +98,9 @@ function EditTripInfo() {
             <option>On foot</option>
             <option>Airplane</option>
             <option>Train</option>
-        </Form.Select>
-      </label>
-    </Row>
+          </Form.Select>
+        </label>
+      </Row>
       <Button type="submit" value="Update Trip" />
     </Form>
   );
