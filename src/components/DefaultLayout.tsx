095a51dc6736cfb973f688f2d5c70e1ce7f8c8fd
FRANCESCA FUSCO s310039 <s310039@studenti.polito.it>
2023-10-31 00:08:31 -0500

    fixed Login.tsx and Register.tsx

--- src/components/DefaultLayout.tsx
+++ src/components/DefaultLayout.tsx
@@ -1,58 +1,57 @@
-import { useContext, useEffect, useState } from "react";
-import { Row, Col, Card } from "react-bootstrap";
-import AppContext from "../context/appContext.tsx";
-import { useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from 'react';
+import { Row, Col, Card } from 'react-bootstrap';
+import AppContext from '../context/appContext';
+import { useNavigate } from 'react-router-dom';
+
 function DefaultLayout(props: { userEmail: string }) {
   const [trips, setTrips] = useState([]);
   const { userEmail } = props;
 
   // Initialize Supabase client
-  const supabase = useContext(AppContext);
+  const supabase  = useContext(AppContext);
   const navigate = useNavigate();
+
   useEffect(() => {
-    // Function to fetch trips from the database
     const fetchTrips = async () => {
       if (userEmail) {
         try {
           const { data: tripsData, error } = await supabase
-            .from("trips")
-            .select("group(name)")
-            .eq("user_id", userEmail);
+              .from('trips')
+              .select('group_id, group:group_id (id, name)')
+              .eq('user_id', userEmail);
 
           if (error) throw error;
 
-          // Set the trip names in state
-          setTrips(
-            tripsData.map((trip: { group: { name: any } }) => trip.group.name)
-          );
+          // Set the trips in state
+          setTrips(tripsData.map((trip: { group: { id: any; name: any; }; }) => ({ id: trip.group.id, name: trip.group.name })));
         } catch (error) {
-          console.error("Error fetching trips:", error);
+          console.error('Error fetching trips:', error);
         }
       } else {
-        navigate("/login");
+        navigate('/login');
       }
     };
 
     fetchTrips();
-  }, [userEmail]);
+  }, [userEmail, supabase, navigate]);
 
   return (
-    <>
-      <Row>
-        <Col>
-          <h3>Your Trips</h3>
-          {trips.length > 0 ? (
-            trips.map((tripName, index) => (
-              <Card key={index} style={{ margin: "10px 0" }}>
-                <Card.Body>{tripName}</Card.Body>
-              </Card>
-            ))
-          ) : (
-            <p>No trips found.</p>
-          )}
-        </Col>
-      </Row>
-    </>
+      <>
+        <Row>
+          <Col>
+            <h3>Your Trips</h3>
+            {trips.length > 0 ? (
+                trips.map((trip) => (
+                    <Card key={trip.id} style={{ margin: '10px 0', cursor: 'pointer' }} onClick={() => navigate(`/trips/${trip.id}`)}>
+                      <Card.Body>{trip.name}</Card.Body>
+                    </Card>
+                ))
+            ) : (
+                <p>No trips found.</p>
+            )}
+          </Col>
+        </Row>
+      </>
   );
 }
 
