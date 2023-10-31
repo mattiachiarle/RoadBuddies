d34ba3d60da677f3a2a0d216289887971e1d8658
MATTIA CHIARLE s308472 <s308472@studenti.polito.it>
2023-10-30 19:02:29 -0500

    Fixed context bug Edited README

--- README.md
+++ README.md
@@ -8,4 +8,75 @@
 
 **Alessandro Martinolli** (amart409@uic.edu)
 
-Link to project pitch (checkpoint 1): https://roadbuddies-static.onrender.com
+## **Architecture**
+
+### **[Excalidraw Schema](https://excalidraw.com/#room=f10af5de26ece127e176,UnIVdGmzzvY2r0Riw1S62Q)**
+
+### **Components:**
+
+- **Browser**: This acts as the client interface, sending requests to either our server or third-party services via APIs. It's essentially where the frontend is executed.
+- **Server**: Our central hub, housing the backend operations. It liaises with databases and offers services and content to the clients.
+- **chat.db**: A dedicated server for storing chat histories.
+- **packingList.db**: This database houses lists of necessary travel items along with their status, specifying whether or not someone has attended to them.
+- **groups.db**: Manages records pertaining to group participants.
+- **External Services**: We're integrating several external platforms for enhanced functionality:
+  - **chatGPT**: To analyze packing lists, suggesting any missing items, and aid in financial decisions among group members.
+  - **Spotify**: For shared playlist creation.
+  - **\*\***Google Calendar:**\*\*** For common itineraries
+  - **PayPal**: Though still under consideration, it's for money exchange between participants.
+  - **Splitwise**: Also being evaluated, its primary use would be to monitor shared expenses.
+
+## **External APIs**
+
+- **chatGPT**:
+  - Offer suggestions on missing items from packing lists.
+  - Help determine who's responsible for the next payment.
+- **Spotify**:
+  - Facilitate the creation of shared playlists.
+- ******\*\*******\*\*\*\*******\*\*******Google Calendar:******\*\*******\*\*\*\*******\*\*******
+  - Share itineraries
+- **PayPal** (Potential inclusion):
+  - Enable monetary exchanges between members.
+- **Splitwise** (Potential inclusion):
+  - Monitor and log shared group expenses.
+
+<aside>
+💡 We might eventually decide not to use Splitwise APIs in case we  simulate its functions by using ChatGPT APIs along with some algorithm of our choice in order to decide who has to pay next based on a history of previous payments (e.g. ***********************credit card roulette*********************** as suggested by the professor).
+
+</aside>
+
+## Hosting
+
+In order to host the application's front-end and back-end we are going to use Render while for the database we are going Supabase.
+
+## Minimum Viable Project
+
+Our project consists in an agumented Group DefaultLayout Application.
+
+**Base setup of the application**
+
+- The user will be able to Sign up and Sign in into the application.
+- The user will be able to create a new DefaultLayout, adding the starting point, the destination, the dates and the partecipants.
+
+**DefaultLayout manager**
+
+- The user will be able to add or remove partecipants to/from the trip.
+- The user will be able to create a list of the items that are neeeded for the trip.
+- The system will keep track of which items each person is bringing and which items are not being brought by individuals.
+- The user will be able to communicate with the other members through a group chat.
+- The system will store the chat history
+- The user will be able to insert the expences he has made during the trip.
+
+**Expense management**
+
+- The most important step of the MVP will be the implementation and integration of the technology of ChatGPT 3.5, thanks to that the user is going to be able to know if any item is missing and which of the participants has to pay for the next expence.
+
+## Technologies used
+
+### Frontend
+
+We think that our project idea can be implemented with a single-page application. The most suitable frontend technology to create it is Vite. For the framework, we decided to use React. Lastly, we'll use Typescript.
+
+### Backend
+
+For the backend, we'll use the Express framework. As DB, we'll use Postgres.
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import ReactMarkdown from "react-markdown";
 
 import NavigationBar from "./components/NavigationBar.tsx";
 import { Col, Container, Row } from "react-bootstrap";
@@ -12,58 +10,54 @@ import EditParticipants from "./components/EditParticipants.tsx";
 import EditTripInfo from "./components/EditTripInfo.tsx";
 import EditToDo from "./components/EditToDo.tsx";
 import Login from "./components/Login.tsx";
-import Register from "./components/Register.ts";
+import Register from "./components/Register.tsx";
 import Chat from "./components/Chat.tsx";
+
+import { AppContextProvider } from "./context/appContext.tsx";
+
 function App() {
-  const [content, setContent] = useState("");
-
-  useEffect(() => {
-    fetch("DESCRIPTION.md")
-      .then((res) => res.text())
-      .then((text) => setContent(text));
-  }, []);
-
   return (
     <>
-      <BrowserRouter>
-        <Container fluid className="App">
-          <ReactMarkdown children={content} />
-          <Row>
-            <NavigationBar />
-          </Row>
-          <Row>
-            <Col>
-              <Routes>
-                <Route path="/" element={<DefaultLayout />}>
-                  {" "}
-                  {/*here the list of trips?*/}
-                  <Route index element={<DefaultLayout />} />
-                  <Route path="/trips/:tripId/" element={<Trip />}>
+      <AppContextProvider>
+        <BrowserRouter>
+          <Container fluid className="App">
+            <Row>
+              <NavigationBar />
+            </Row>
+            <Row>
+              <Col>
+                <Routes>
+                  <Route path="/" element={<DefaultLayout />}>
                     {" "}
-                    {/*here the trip sidebar?*/}
-                    <Route index element={<TripInfo />} />
-                    <Route
-                      path="editParticipants"
-                      element={<EditParticipants />}
-                    />{" "}
-                    {/*here the edit participants?*/}
-                    <Route path="editInfo" element={<EditTripInfo />} />{" "}
-                    <Route path="chat" element={<Chat />} />{" "}
-                    {/*here the edit form?*/}
-                    <Route path="editToDo" element={<EditToDo />} />{" "}
-                    {/*here the edit route?*/}
+                    {/*here the list of trips?*/}
+                    <Route index element={<DefaultLayout />} />
+                    <Route path="/trips/:tripId/" element={<Trip />}>
+                      {" "}
+                      {/*here the trip sidebar?*/}
+                      <Route index element={<TripInfo />} />
+                      <Route
+                        path="editParticipants"
+                        element={<EditParticipants />}
+                      />{" "}
+                      {/*here the edit participants?*/}
+                      <Route path="editInfo" element={<EditTripInfo />} />{" "}
+                      <Route path="chat" element={<Chat />} />{" "}
+                      {/*here the edit form?*/}
+                      <Route path="editToDo" element={<EditToDo />} />{" "}
+                      {/*here the edit route?*/}
+                    </Route>
+                    <Route path="login" element={<Login />} />{" "}
+                    {/*here the login form?*/}
+                    <Route path="register" element={<Register />} />{" "}
+                    {/*here the register form?*/}
                   </Route>
-                  <Route path="login" element={<Login />} />{" "}
-                  {/*here the login form?*/}
-                  <Route path="register" element={<Register />} />{" "}
-                  {/*here the register form?*/}
-                </Route>
-              </Routes>
-            </Col>
-            <Col></Col>
-          </Row>
-        </Container>
-      </BrowserRouter>
+                </Routes>
+              </Col>
+              <Col></Col>
+            </Row>
+          </Container>
+        </BrowserRouter>
+      </AppContextProvider>
     </>
   );
 }
